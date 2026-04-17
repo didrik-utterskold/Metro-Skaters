35166bc32552b1d1cadc854e7e00caede2a938b3
didrik-utterskold <utterskold.didrik@gmail.com>
2026-04-17 17:34:38 +0200

    Fixes #16
    Added Coins! The coins play a sound effect and destroy themselves when the player collects them.
    
    All that is needed now is to make the Coin Counter update...
    
    A problem for another time! :)

---
+++ Assets/Scripts/Collectable.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Rotates the collectable every frame to make it more visually appealing
+    private void Update()
+    {
+        transform.Rotate(50f * Time.deltaTime * Vector3.up);
+    }
+
+     private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Collect();
+        }
+    }
+
+    private void Collect()
+    {
+        AudioSource.PlayClipAtPoint(audioSource.clip, transform.position);
+        Destroy(gameObject);
+    }
+}
