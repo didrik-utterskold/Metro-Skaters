af0d5bffb90adfa49aef2be768bc832af64fa0d8
didrik-utterskold <utterskold.didrik@gmail.com>
2026-04-20 19:55:59 +0200

    Fixes #19
    The Score system has been updated!
    
    Should be working quite cleanly.

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -2,11 +2,15 @@ using UnityEngine;
 
 public class ScoreManager : MonoBehaviour
 {
-    private int CoinCount;
+    private int coinCount;
 
-    private float Score;
+    private float currentScore;
+
+    private float highScore;
 
     private float currentMultiplier;
+
+    private float elapsedTime;
 
     public static ScoreManager Instance;
 
@@ -17,30 +21,43 @@ public class ScoreManager : MonoBehaviour
 
     private void Start()
     {
-        CoinCount = 0;
-        Score = 0f;
+        coinCount = 0;
+        currentScore = 0f;
         currentMultiplier = 1f;
+        highScore = PlayerPrefs.GetFloat("HighScore", 0f);
     }
 
     private void Update()
     {
-        Score = Time.time * currentMultiplier * 100;
+        elapsedTime += Time.deltaTime;
+        currentScore = elapsedTime * currentMultiplier * 100;
+
+        if (currentScore > highScore)
+        {
+            highScore = currentScore;
+        }
+    }
+
+    public void SaveHighScore()
+    {
+        PlayerPrefs.SetFloat("HighScore", highScore);
+        PlayerPrefs.Save();
     }
 
     // Getters and Setters
     public int GetCoinCount()
     {
-        return CoinCount;
+        return coinCount;
     }
 
     public void AddCoin()
     {
-        CoinCount++;
+        coinCount++;
     }
 
-    public int GetScore()
+    public float GetScore()
     {
-        return (int)Score;
+        return currentScore;
     }
 
     public float GetMultiplier()
